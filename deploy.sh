9cd5bd5a10379894421633a81d5404db1894e4a5
Mariano Gappa <mariano@movio.co>
2016-09-03 18:12:58 -0300

    Checks if $DOTFILES exists.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 # Fail on error; echo each ran line and expand any env variables
-set -evx
+set -e
 
-ln -sf "$DOTFILES/.bashrc" $HOME/.bashrc
-ln -sf "$DOTFILES/.bash_profile" $HOME/.bash_profile
+if [ "$DOTFILES" = "" ]
+then
+    echo '$DOTFILES is not set.'
+    exit 1
+fi
+
+(
+    set -vx
+    ln -sf "$DOTFILES/.bashrc" $HOME/.bashrc
+    ln -sf "$DOTFILES/.bash_profile" $HOME/.bash_profile
+)
+
+echo -e "\nOK! The script reached here, which means it succeeded!\n"
