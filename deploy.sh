c1f9c438552d9cb3ce2383e99b3345035330fb99
Mariano Gappa <mariano@movio.co>
2016-09-03 18:42:56 -0300

    Adds gitconfig and gitignore_global

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,8 @@ fi
     set -vx
     ln -sf "$DOTFILES/.bashrc" $HOME/.bashrc
     ln -sf "$DOTFILES/.bash_profile" $HOME/.bash_profile
+    ln -sf "$DOTFILES/.gitconfig" $HOME/.gitconfig
+    ln -sf "$DOTFILES/.gitignore_global" $HOME/.gitignore_global
 )
 
 echo -e "\nOK! The script reached here, which means it succeeded!\n"
